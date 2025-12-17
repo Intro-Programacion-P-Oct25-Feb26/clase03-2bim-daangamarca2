31bb47d569fb5dd0d3458c1aeee1277d61c156d9
daangamarca2 <arturoangamarca120@gmail.com>
2025-12-17 12:57:08 -0500

    Ejercicio-Final

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -25,8 +25,8 @@ public class Ejercicio {
         for (int i = 0; i < 5; i++) {
             System.out.println("Ingrese el nombre del encargado de la estación ");
             nombresClientes[i] = entrada.nextLine();
-
             double sumaEstacion = 0;
+            
             for (int j = 0; j < 12; j++) {
                 System.out.println("Mes " + (j + 1));
                 produccionMensual[i][j] = entrada.nextDouble();
@@ -35,5 +35,27 @@ public class Ejercicio {
             totalMensual[i] = sumaEstacion;
             entrada.nextLine();
         }
+
+        double produccionTotal = totalMensual[0];
+        int indice = 0;
+
+        for (int i = 0; i < 5; i++) {
+            if (totalMensual[i] > produccionTotal) {
+                produccionTotal = totalMensual[i];
+                indice = i;
+            }
+        }
+
+        System.out.println("\nANALISIS DE PRODUCCION FINAL");
+        System.out.println("ESTACION");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("%s Estación %d - Total Producción: $ %.2f\n",
+                    nombresClientes[i], i+1, totalMensual[i]);
+        }
+        System.out.printf("\nEstación más productiva: %s\nEncargado de la "
+                + "estación: %s\nCantidad de la estación más productiva: $ "
+                + "%.2f\n",nombresClientes[indice], nombresClientes[indice],
+                produccionTotal);
     }
 }
